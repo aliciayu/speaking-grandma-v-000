ec4f4f02f34931e17fb9520cca81c5c51489bcc7
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-28 02:05:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase()
-    "HUH?! SPEAK UP, SONNY!"
+  if phrase == phrase.upcase()
+    "NO, NOT SINCE 1938!"
     else
-      "NO, NOT SINCE 1938!"
+      "HUH?! SPEAK UP, SONNY!"
     end
   elsif phrase=="I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
