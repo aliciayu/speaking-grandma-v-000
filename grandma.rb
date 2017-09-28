2b88dfb77e179b3e671624aef23b658940a3cd04
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-28 02:06:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
     else
       "HUH?! SPEAK UP, SONNY!"
-    end
-  elsif == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+  
   end
 end
 
