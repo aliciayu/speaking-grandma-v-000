7269098bbdb52a12d8eb474a2220e84895fd3538
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-28 02:06:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase()
     "NO, NOT SINCE 1938!"
-    else
-      "HUH?! SPEAK UP, SONNY!"
-  
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
